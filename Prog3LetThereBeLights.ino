8f6788f1095b1b76babfed0bd352390b309b99cf
LukeCarlson <42789571+Skylake101@users.noreply.github.com>
2021-04-05 12:31:27 -0500

    Update Prog3LetThereBeLights.ino

--- Prog3LetThereBeLights.ino
+++ Prog3LetThereBeLights.ino
@@ -1,5 +1,9 @@
 /*
  * Author: Luke Carlson
+ * My photon came with an RGB component that uses red, green and blue
+ * Each of these colors can be set between 0-255 (Read particles API to learn more about the firmware)
+ * https://docs.particle.io/reference/device-os/firmware/photon/
+ * I set this program to flash 10 random colors with a 1 second rest time in between
  */
  const int dell = 100; //Start of main loop using del1 as your term
  int j = 0;
